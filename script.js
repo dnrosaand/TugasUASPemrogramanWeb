913865b59ee9f1003afdb2ec62471a21be73e12c
dnrosaand <dindarosaa30@gmail.com>
2026-07-26 16:40:03 +0700

    Menambahkan halaman detail terpisah untuk Flash Sale dan Our Stock

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ if (userBtn) {
         window.location.href = "login.html";
     });
 }
+
+function goToDetail(card){
+
+    const product = {
+        image: card.dataset.image,
+        category: card.dataset.category,
+        artist: card.dataset.artist,
+        name: card.dataset.name,
+        price: card.dataset.price,
+        oldprice: card.dataset.oldprice,
+        sold: card.dataset.sold,
+        stock: card.dataset.stock,
+        desc: card.dataset.desc
+    };
+
+    localStorage.setItem("product", JSON.stringify(product));
+    window.location.href = "detail.html";
+}
+
+const minusBtn = document.getElementById("minus");
+const plusBtn = document.getElementById("plus");
+const qtyInput = document.getElementById("qty");
+
+plusBtn.addEventListener("click", function () {
+    qtyInput.value = parseInt(qtyInput.value) + 1;
+});
+
+minusBtn.addEventListener("click", function () {
+    if (parseInt(qtyInput.value) > 1) {
+        qtyInput.value = parseInt(qtyInput.value) - 1;
+    }
+});
+
