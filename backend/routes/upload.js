16133194da9c856ea1fbd44986335e14a7136817
dnrosaand <dindarosaa30@gmail.com>
2026-07-29 17:58:33 +0700

    test profil

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -1,12 +1,20 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadPath = path.join(__dirname, "../uploads");
+
+// kalau folder belum ada, buat otomatis
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null, "uploads/");
+    destination(req, file, cb) {
+        cb(null, uploadPath);
     },
 
-    filename: function(req, file, cb){
+    filename(req, file, cb) {
         const ext = path.extname(file.originalname);
         cb(null, Date.now() + ext);
     }
