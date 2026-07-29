27b691cb1d7c6cd8839c4ad48b6b14cb9cb8b25e
dnrosaand <dindarosaa30@gmail.com>
2026-07-29 23:25:57 +0700

    flashsale

--- frontend/flashsale.js
+++ frontend/flashsale.js
@@ -103,3 +103,34 @@ async function loadProducts() {
 
 
 loadProducts();
+
+// =======================
+// DETAIL PRODUK
+// =======================
+
+function goToDetail(card) {
+
+    const product = {
+
+        image: card.dataset.image,
+        category: card.dataset.category,
+        artist: card.dataset.artist,
+        name: card.dataset.name,
+        price: card.dataset.price,
+        oldprice: card.dataset.oldprice,
+        sold: card.dataset.sold,
+        stock: card.dataset.stock,
+        desc: card.dataset.desc
+
+    };
+
+
+    localStorage.setItem(
+        "product",
+        JSON.stringify(product)
+    );
+
+
+    window.location.href = "detail.html";
+
+}
